6eb2aae9a19517d7a77df4be49e687a826f8a611
miketsai05 <34243790+miketsai05@users.noreply.github.com>
2021-04-27 21:54:39 -0700

    'Updated from urllib to requests library'

--- extract_SEC_data.py
+++ extract_SEC_data.py
@@ -1,14 +1,9 @@
 # LOOP THROUGH EACH URL AND EXTRACT LVL3 INVESTMENT DATA
 
-#pip install secedgar
-
-#from secedgar.filings import Filing, FilingType
-#from secedgar.cik_lookup import CIKLookup
-
-#import urllib
 import requests
 from os import path
 import time
+import pyautogui
 
 import pandas as pd
 import xml.etree.ElementTree as ET
@@ -126,12 +121,19 @@ def main():
     for ind in master_urls.index:
 
         if master_urls.loc[ind,'CIK'] != lastCIK: #comment out
-            print(master_urls.loc[ind,'CIK'])
-
-        if ind%50 == 0:
-            print(ind)
+            print(master_urls.loc[ind,'CIK']) #comment out
 
         if master_urls.loc[ind,'accessNum'] == None:
+
+            if ind % 100 == 0:
+                print(ind)  # comment out
+                pyautogui.press('volumedown')
+                time.sleep(0.5)
+                pyautogui.press('volumeup')
+
+            if ind % 1000 == 0:
+                master_urls.to_pickle(urlfilename)
+                master_holdings.to_pickle(holdingsfilename)
 
             #req = urllib.request.Request(master_urls.loc[ind,'filingURL'], headers={'User-Agent': 'Mozilla/5.0'})
             #data1 = urllib.request.urlopen(req).read().decode('utf-8')
@@ -146,7 +148,7 @@ def main():
 
             master_holdings = master_holdings.append(holdings, ignore_index = True)
 
-            time.sleep(1)
+        time.sleep(0.2) #SEC rate limit 10 requests / sec - slight buffer to be safe here
 
         lastCIK = master_urls.loc[ind,'CIK'] #comment out
 
@@ -157,121 +159,3 @@ if __name__ == "__main__":
     main()
 
 
-### PREVIOUS SCRATCH CODE
-
-# Look up list of filing URLs
-
-#fund_name = 'T. Rowe Price Science & Technology Fund, Inc.'
-#cik = CIKLookup(['T. Rowe Price Science & Technology Fund, Inc.'])
-
-# test_filing = Filing(cik,filing_type=FilingType.FILING_NPORT, start_date='20200630', count=2)
-
-#test_url = 'https://www.sec.gov/Archives/edgar/data/819930/000175272420248026/0001752724-20-248026.txt'
-
-
-# all_filings = []
-# all_holdings = []
-#
-#
-#
-#
-# for url in all_urls[ciks[70]]:
-#
-#     file = urllib.request.urlopen(url)
-#     data1 = urllib.request.urlopen(url).read().decode('utf-8')
-#
-#     # Getting general filing information
-#
-#     tag = "<genInfo>"
-#     data2 = getSection(data1, tag)
-#     info_cols = ["regName", "regFileNumber", "regCik", "regLei", "regStreet1", "regCity", "regZipOrPostalCode",
-#                  "regPhone", "seriesName", "seriesId", "seriesLei", "repPdEnd", "repPdDate", "isFinalFiling"]
-#
-#     text_root = ET.fromstring(data2)
-#     filingInfo = []
-#
-#     for el in info_cols:
-#         if text_root.find(el) is not None:
-#             filingInfo.append(text_root.find(el).text)
-#         else:
-#             filingInfo.append(None)
-#
-#     acc_num = getLine(data1,"ACCESSION NUMBER:")
-#     curr_date = curr_filing["repPdDate"]
-#
-#     curr_filing = {info_cols[i]: filingInfo[i] for i, _ in enumerate(info_cols)}
-#     curr_filing["FilingURL"] = url
-#     curr_filing["accessNum"] = acc_num
-#     curr_filing["fileDate"] = getLine(data1, "FILED AS OF DATE:")
-#     all_filings.append(curr_filing)
-#
-#     # Getting holding level information
-#
-#     tag = "<invstOrSecs>"
-#     data2 = getSection(data1, tag)
-#
-#     df_cols = ["name", "lei", "title", "cusip", "balance", "units", "curCd", "valUSD", "pctVal", "payoffProfile",
-#                "assetCat", "issuerCat", "invCountry", "isRestrictedSec", "fairValLevel", ]
-#     id_cols = ["isin", "ticker", "other"]
-#     sec_lend_cols = ["isCashCollateral", "isNonCashCollateral", "loanByFundCondition", "isLoanByFund", "loanVal"]
-#
-#     twolevel_cols = {"identifiers": id_cols, "securityLending": sec_lend_cols}
-#
-#     rows = xml2list(data2, df_cols, twolevel_cols)
-#
-#     for item in rows:
-#       item.update({"ValuationDate": curr_date, "accessNum": acc_num})
-#
-#     all_holdings = all_holdings + rows
-#
-# all_filings = pd.DataFrame(all_filings, columns=info_cols + ["FilingURL", "accessNum", "fileDate"])
-#
-# all_holdings = pd.DataFrame(all_holdings, columns=df_cols + id_cols + sec_lend_cols + ["ValuationDate", "accessNum"])
-#
-# print(all_filings.head())
-# print(all_holdings.head())
-
-# level3 = all_holdings[all_holdings["fairValLevel"] == "3"]
-
-# print(all_holdings.size)
-# all_holdings.head(5)
-
-# print(level3.size)
-# level3.head()
-
-# len(level3)
-# print(level3.ValuationDate)
-
-# # Get security level info - TESTING ONLY
-#
-# tag = "<invstOrSecs>"
-# data2 = getSection(data1, tag)
-#
-# df_cols = ["name", "lei", "title", "cusip", "balance", "units", "curCd", "valUSD", "pctVal", "payoffProfile",
-#            "assetCat", "issuerCat", "invCountry", "isRestrictedSec", "fairValLevel", ]
-# id_cols = ["isin", "ticker", "other"]
-# sec_lend_cols = ["isCashCollateral", "isNonCashCollateral", "loanByFundCondition", "isLoanByFund", "loanVal"]
-#
-# twolevel_cols = {"identifiers": id_cols, "securityLending": sec_lend_cols}
-#
-# rows = xml2list(data2, df_cols, twolevel_cols, curr_date)
-#
-# test = pd.DataFrame(rows, columns=df_cols + id_cols + sec_lend_cols + ["ValuationDate"])
-#
-# test.head(10)
-
-# whos
-
-# TESTING
-# f = open('CIKs/T_Rowe.txt', 'r')
-# all_ciks = f.read()
-# f.close()
-#
-# all_ciks = all_ciks.splitlines()
-# all_ciks = all_ciks[1:]
-#
-# ciks = [item.zfill(10) for item in all_ciks] #CIKLookup(all_ciks)
-#
-# all_urls = Filing(ciks[70], filing_type=FilingType.FILING_NPORTP).get_urls() #break this up into multiple requests
-#
-# print(all_urls)
