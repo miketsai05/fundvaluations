664f36425a80c40b0dcbc1b4f8e818687b537abb
miketsai05 <34243790+miketsai05@users.noreply.github.com>
2021-05-13 19:58:40 -0700

    Finished unicorn app page. Created function to search all records given search term

--- app.py
+++ app.py
@@ -1,78 +1,127 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table as dt
+from dash_table import FormatTemplate
+from dash_table.Format import Format, Scheme
+import dash_bootstrap_components as dbc
 import pandas as pd
-from dash.dependencies import Input, Output
 import selectData as sd
 
+def gen_table(unicorn_name):
+    tmptable = unicorn_data[unicorn_data['unicorn']==unicorn_name][cols].copy()
+    tmptable['Fund'] = '['+tmptable['Fund'].astype(str)+']('+tmptable['filingURL'].astype(str)+')'
+    tmptable['valDate'] = tmptable['valDate'].dt.date
+    return tmptable
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.LUMEN] # ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-# df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
-# fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-#                  size="population", color="continent", hover_name="country",
-#                  log_x=True, size_max=60)
-
-fig = sd.gen_fig('Bytedance')
 
 unicornsfilename = 'master_unicorns.xlsx'
 master_unicorns = pd.read_excel(unicornsfilename)
 master_unicorns = master_unicorns.where(pd.notnull(master_unicorns), None)
-
-moneyformat = dt.FormatTemplate.money(2)
-numformat = dt.Format(precision=0, scheme=dt.Scheme.fixed).group(True)
+unicornset = master_unicorns.loc[0:30,'Company Name']
+unicorn_data = pd.read_pickle('unicorn_data')
 
 cols = ['unicorn', 'fundManager', 'valDate', 'pershare', 'balance', 'Fund', 'name', 'title', 'filingURL']
 colnames = ['Company', 'Fund Manager', 'Valuation Date', 'Per Share Valuation', 'Number of Shares', 'Fund', 'Holding Name', 'Holding Title']
 coltype = ['text', 'text', 'datetime', 'numeric', 'numeric', 'text', 'text', 'text']
 colpresentation = ['input', 'input', 'input', 'input', 'input', 'markdown', 'input', 'input']
+
 dt_cols = [{'name':x, 'id':y, 'type':z, 'presentation':a} for x, y, z, a in zip(colnames, cols[:-1], coltype, colpresentation)]
 
+moneyformat = FormatTemplate.money(2)
+numformat = Format(precision=0, scheme=Scheme.fixed).group(True)
 
-unicorn_data = pd.read_pickle('unicorn_data')
-
-def gen_table(unicorn_name):
-    tmptable = unicorn_data[unicorn_data['unicorn']==unicorn_name][cols].copy()
-    tmptable['Fund'] = '['+tmptable['Fund'].astype(str)+']('+tmptable['filingURL'].astype(str)+')'
-    return tmptable
+dt_cols[3]['format'] = moneyformat
+dt_cols[4]['format'] = numformat
 
 
-table1 = gen_table('Bytedance')
 
 app.layout = html.Div([
 
-    html.H1(
-        'SEC Reported Valuations',
-        style={'textAlign': 'center'}
+    dcc.Store(id='table-memory'),
+
+    # html.H1(
+    #     'SEC Reported Valuations',
+    #     style={'textAlign': 'center'}
+    # ),
+
+    dbc.NavbarSimple([
+        dbc.NavItem(dbc.NavLink("Home", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("Summary List", href="#"),
+                dbc.DropdownMenuItem("Individual", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label='Unicorns',
+        ),
+        dbc.NavItem(dbc.NavLink('Search All',href='#'))
+        ],
+        brand="SEC Reported Valuations",
+        brand_style={'font-size': 32},
+        brand_href="#",
+        color="primary",
+        dark=True,
+        sticky='top',
+        style={'font-size': 18}
     ),
 
     html.Div([
         html.Label('Company Name'),
         dcc.Dropdown(
             id='company-input',
-            options=[{'label': x, 'value': x} for x in master_unicorns.loc[0:30,'Company Name']],
-            value='Bytedance')]
-    ,style={'padding-top': '5%', 'width': '20%', 'display': 'inline-block', 'vertical-align': 'top'}),
+            options=[{'label': x, 'value': x} for x in unicornset],
+            value='Bytedance',
+            clearable=False
+        )
+        ],
+        style={'padding-top': '5%', 'width': '20%', 'display': 'inline-block', 'vertical-align': 'top'}
+    ),
 
     html.Div(
         dcc.Graph(
-            id='valuations-graph',
-            figure=fig
-        )
-    ,style={'width': '79%', 'display': 'inline-block', 'vertical-align': 'top'}),
+            id='valuations-graph'
+        ),
+        style={'width': '79%', 'display': 'inline-block', 'vertical-align': 'top'}
+    ),
+
+    html.Div(
+        dbc.DropdownMenu(
+            dbc.Checklist(
+                id='filterManager',
+                options=[],
+                value=[],
+            ),
+            label='Filter Table by Fund Manager',
+        ),
+        style={'width': 'auto', 'display': 'inline-block', 'vertical-Align': 'top', 'padding-left': '20px', 'padding-right': '10px'}
+    ),
+
+    html.Div(
+        dbc.DropdownMenu(
+            dbc.Checklist(
+                id='filterDate',
+                options=[],
+                value=[],
+            ),
+            label='Filter Table by Valuation Date',
+        ),
+        style={'width': 'auto', 'display': 'inline-block', 'vertical-Align': 'top', 'padding-bottom': '20px'}
+    ),
+
 
     html.Div(
         dt.DataTable(
-            id = 'table1',
-            columns = dt_cols,
-            data = table1.to_dict('records')
+            id='table1',
+            columns=dt_cols,
+            style_header={'fontWeight': 'bold', 'whiteSpace': 'normal', 'padding-left': '5px', 'padding-right': '5px'},
+            style_cell={'textAlign': 'center'},
+            sort_action='native',
         )
     )
 
@@ -80,23 +129,37 @@ app.layout = html.Div([
 
 
 
-
-
 # add toggle to change size of points on graph
 
 @app.callback(
     Output(component_id='valuations-graph', component_property='figure'),
+    Output(component_id='table-memory', component_property='data'),
+    Output(component_id='filterManager', component_property='options'),
+    Output(component_id='filterManager', component_property='value'),
+    Output(component_id='filterDate', component_property='options'),
+    Output(component_id='filterDate', component_property='value'),
     Input(component_id='company-input', component_property='value')
 )
 def update_graph(input_value):
-    return sd.gen_fig(input_value)
+    tmptable = gen_table(input_value)
+    manageroptions = [{'label': i, 'value': i} for i in sorted(tmptable['fundManager'].unique())]
+    managerval = sorted(tmptable['fundManager'].unique())
+    dateoptions = [{'label': i, 'value': i} for i in sorted(tmptable['valDate'].unique(), reverse=True)]
+    dateval = sorted(tmptable['valDate'].unique(), reverse=True)
+    return sd.gen_fig(input_value), tmptable.to_dict('records'), manageroptions, managerval, dateoptions, dateval
+
 
 @app.callback(
     Output(component_id='table1', component_property='data'),
-    Input(component_id='company-input', component_property='value')
+    Input(component_id='filterManager', component_property='value'),
+    Input(component_id='filterDate', component_property='value'),
+    Input(component_id='table-memory', component_property='data')
 )
-def update_table(input_value):
-    return gen_table(input_value).to_dict('records')
+def filter_table(selectmanager, selectdate, data):
+    tmptable = pd.DataFrame.from_dict(data)
+    tmptable = tmptable[tmptable['fundManager'].isin(selectmanager) & tmptable['valDate'].isin(selectdate)]
+    return tmptable.to_dict('records')
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- selectData.py
+++ selectData.py
@@ -1,13 +1,7 @@
 # FOR GIVEN COMPANY - FIND RELEVANT SEC FILING DATA FROM MASTER_HOLDINGS.PKL
 
-# set up pages with logos for all unicorns
-#   doing top 30 first
+# set up pages with logos for all unicorns - doing top 30 first
 # set up page for all other searches
-
-# load master_unicorns
-
-# match by searching name string for Company Name and AKAs
-# fuzzy match legal name
 
 import pandas as pd
 import plotly
@@ -15,51 +9,24 @@ import plotly.express as px
 import numpy as np
 
 
-def find_records(co_name, aka, legal_name, search_legal=False, threshold=0.5):
-    ''' Looks up relevant records from master_holdings.pkl'''
+def search_select(search_name):
+    """ Looks up relevant records from master_holdings.pkl given search term """
 
     # Should I pass in master_holdings as a dataframe rather than open each time?? TBD
 
     holdingsfilename = "master_holdings.pkl"
     master_holdings = pd.read_pickle(holdingsfilename)
 
-    tmp1 = master_holdings[master_holdings['name'].str.lower().str.contains(co_name.lower())]
-    tmp2 = pd.DataFrame(columns=master_holdings.columns)
-    tmp3 = pd.DataFrame(columns=master_holdings.columns)
+    ind1 = master_holdings['name'].str.lower().str.contains(search_name.lower())
+    ind2 = master_holdings['title'].str.lower().str.contains(search_name.lower())
 
-    if aka:
-        aka_list = [item.strip() for item in aka.split(',')]
-        for short_name in aka_list:
-            tmp2.append(master_holdings[master_holdings['name'].str.lower().str.contains(short_name.lower())])
+    search_results = master_holdings[ind1|ind2]
 
-    # if search_legal:
-        # to do - implement fuzzy search??
-
-    all_records = tmp1.append(tmp2).append(tmp3)
-    all_records = all_records.drop_duplicates()
-
-    return all_records
+    return search_results
 
 
-def select_unicorns():
-
-    unicornsfilename = 'master_unicorns.xlsx'
-    master_unicorns = pd.read_excel(unicornsfilename)
-    master_unicorns = master_unicorns.where(pd.notnull(master_unicorns), None)
-
-    tmp_list = []
-
-    for i in range(0, 30):
-        co_name = master_unicorns.loc[i, 'Company Name']
-        aka = master_unicorns.loc[i, 'aka']
-        legal_name = master_unicorns.loc[i, 'Legal Name']
-
-        tmp = find_records(co_name, aka, legal_name)
-        tmp['unicorn'] = co_name
-
-        tmp_list.append(tmp)
-
-    select_holdings = pd.concat(tmp_list, ignore_index=True)
+def merge_data(select_holdings):
+    """ Merges holding data with URL data and CIK data """
 
     holdings_cols = ['accessNum', 'name', 'title', 'balance', 'curCd', 'valUSD', 'unicorn']
     cik_cols = ['CIK', 'fundManager', 'Fund']
@@ -83,13 +50,72 @@ def select_unicorns():
 
     unicorn_data['pershare'] = (unicorn_data['valUSD']/unicorn_data['balance']).round(2)
 
-    # Concatenate Fund and Series before groupby
+
+def group_data(merged_data):
+    """ Groups merged data for better visual in plotly graph. Converts certain columns and normalizes balance data
+      From select_unicorns: AccessNum, Name, Title, balance, curCd, valUSD, Unicorn Name
+      From master_urls: CIK, filingURL, AccessNum, valDate, fileDate (ALL COLUMNS)
+      From master_ciks: CIK, Manager Name, Fund """
+
+    # Concatenate Fund and Series before groupby?
 
     cols = ['unicorn', 'fundManager', 'valDate', 'pershare']
     f = {'balance': 'sum', 'Fund': ',<br>'.join, 'name':','.join, 'title':','.join, 'filingURL':','.join}
 
-    unicorn_data_grouped = unicorn_data.groupby(cols, as_index=False).agg(f)
-    unicorn_data_grouped['normbalance'] = np.log10(unicorn_data_grouped['balance']) # unicorn_data_grouped['balance']**0.35
+    data_grouped = merged_data.groupby(cols, as_index=False).agg(f)
+    data_grouped['normbalance'] = np.log10(data_grouped['balance'])
+
+
+def search_unicorns(co_name, aka, legal_name, search_legal=False, threshold=0.5):
+    """ Searches for relevant unicorn records in master_holdings.pkl given unicorn name, aka or legal name """
+
+    # Should I pass in master_holdings as a dataframe rather than open each time?? TBD
+
+    holdingsfilename = "master_holdings.pkl"
+    master_holdings = pd.read_pickle(holdingsfilename)
+
+    ind1 = master_holdings['name'].str.lower().str.contains(co_name.lower())
+    ind2 = pd.Series(np.full(len(master_holdings), False, dtype=bool), index=master_holdings.index)
+    # ind3 = ind2
+
+    if aka:
+        aka_list = [item.strip() for item in aka.split(',')]
+        for short_name in aka_list:
+            ind2 = ind2 | (master_holdings['name'].str.lower().str.contains(short_name.lower()))
+
+    # if search_legal:
+        # to do - implement fuzzy search??
+
+    unicorn_records = master_holdings[ind1|ind2]
+
+    return unicorn_records
+
+
+def select_unicorns():
+    """ Loops through unicorns in master_unicorns.xlsx - searches for records.
+    Concats all records, merges with URL and CIK data and groups by name, fund manager, valdate and price.
+    Saves both selected merged records and grouped data"""
+
+    unicornsfilename = 'master_unicorns.xlsx'
+    master_unicorns = pd.read_excel(unicornsfilename)
+    master_unicorns = master_unicorns.where(pd.notnull(master_unicorns), None)
+
+    tmp_list = []
+
+    for i in range(0, 30):
+        co_name = master_unicorns.loc[i, 'Company Name']
+        aka = master_unicorns.loc[i, 'aka']
+        legal_name = master_unicorns.loc[i, 'Legal Name']
+
+        tmp = search_unicorns(co_name, aka, legal_name)
+        tmp['unicorn'] = co_name
+
+        tmp_list.append(tmp)
+
+    select_unicorns = pd.concat(tmp_list, ignore_index=True)
+
+    unicorn_data = merge_data(select_unicorns)
+    unicorn_data_grouped = group_data(unicorn_data)
 
     unicorn_data.to_pickle('unicorn_data')
     unicorn_data_grouped.to_pickle('unicorn_data_grouped')
@@ -158,25 +184,7 @@ def gen_graph(unicorn_name):
     # add annotations for funding round
 
 
-# fig.update_traces(hovertemplate = '<b>%{hovertext}</b><br>Valuation Date = %{x}<br>Per Share Valuation = %{y}<br>Number of Shares = %{hover_data[0]}')
-
 if __name__ == "__main__":
     select_unicorns()
     unicorn_data = pd.read_pickle('unicorn_data')
-    gen_graph('Bytedance')
 
-#import plotly.express as px
-#xxx = select_unicorns[select_unicorns['Unicorn Name']=='Bytedance'].copy()
-#xxx['Per Share']=xxx['valUSD'].astype(float)/xxx['balance'].astype(float)
-#pdata = xxx[['valDate','pershare']].copy()
-#pdata.valDate = pd.to_datetime(pdata.valDate)
-#import plotly
-#plotly.offline.plot(fig, filename='plotlygraph.html')
-#yyy = xxx.groupby(['Manager Name','valDate','pershare','name','title'], as_index=False).agg({'balance':'sum', 'Fund':','.join})
-
-# Data i need:
-# From select_unicorns: AccessNum, Name, Title, balance, curCd, valUSD, Unicorn Name
-# From master_urls: CIK, filingURL, AccessNum, valDate, fileDate (ALL COLUMNS)
-# From master_ciks: CIK, Manager Name, Fund
-
-# CHECK SPACE X
